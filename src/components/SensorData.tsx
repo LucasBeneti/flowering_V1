f36b10fa7f3cbc878364b6a48cedd63ee2cf7b3d
Lucas Beneti <lbeneti@gmail.com>
2022-11-20 01:46:28 -0300

    DataScreen is a WIP

--- src/components/SensorData.tsx
+++ src/components/SensorData.tsx
@@ -1,11 +1,48 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
-const SensorData = () => {
+// TODO implementar algo pra selecionar a unidade da temperatura em uso
+export type PossibleUnits = {
+  temp: string;
+  hum: string;
+  co2: string;
+  light: string;
+};
+
+type SensorDataProps = {
+  value: string;
+  dataName: string;
+};
+
+const DATA: Record<string, Record<string, string>> = {
+  temp: { name: "Temperature", unit: "*C" },
+  hum: { name: "Humidity", unit: "%" },
+  co2: { name: "CO2", unit: "ppm" },
+  light: { name: "Lux", unit: "lux" },
+};
+
+const SensorData = ({ value, dataName }: SensorDataProps) => {
   return (
-    <View>
-      <Text>Data from a sensor</Text>
+    <View style={styles.container}>
+      <Text style={styles.name}>{DATA[dataName]["name"]}</Text>
+      <Text style={styles.value}>{`${value} ${DATA[dataName]["unit"]}`}</Text>
     </View>
   );
 };
 
 export default SensorData;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    alignItems: "center",
+    width: "45%",
+    minHeight: "15%",
+  },
+  name: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  value: {
+    fontSize: 20,
+  },
+});
--- src/routes/screens/DataScreen.tsx
+++ src/routes/screens/DataScreen.tsx
@@ -1,11 +1,56 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+
+import SensorData from "../../components/SensorData";
 
 const DataScreen = () => {
+  // mocked data coming from device
+  const testData = [
+    { dataName: "temp", value: "25" },
+    { dataName: "hum", value: "58" },
+    { dataName: "co2", value: "500" },
+    { dataName: "light", value: "680" },
+  ];
+  // TODO seria interessante ter um mini gráfico pra cara medida
+  // pra que o usuário consiga ver caso tenha alguma alteração muito brusca
+  // e caso deseje, vá pra tela de maior detalhe dos dados fornecidos.
+
+  //TODO: não sei como será a parte de armazenar os dados, talvez teremos que
+  // ter um banco local com eles e manter atualizado com o DB remoto
   return (
-    <View>
-      <Text>DataScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.pageDescription}>
+        Here you’ll be able to see the data available from your device. Click on
+        any of them to see a more detailed view.
+      </Text>
+      <View style={styles.sensorDataContainer}>
+        {testData.map(({ dataName, value }) => (
+          <SensorData value={value} dataName={dataName} key={dataName} />
+        ))}
+      </View>
     </View>
   );
 };
 
 export default DataScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  pageDescription: {
+    fontSize: 16,
+    fontWeight: "600",
+    margin: 20,
+  },
+  sensorDataContainer: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: "100%",
+    backgroundColor: "#ddd",
+    justifyContent: "center",
+    paddingTop: 20,
+  },
+});
